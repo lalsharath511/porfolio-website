16436e7e3251ccad4a23397c65ba33a2e99374fc
lalsharath511 <lalsharath511@gmail.com>
2022-12-08 16:26:48 +0530

    debuge false commit

--- project/settings.py
+++ project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-w#7hjaij2v_vja1old+1!mm@8-v3zzcw90!4c0k$z_c@ht*vmt"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['porfolio-website-production.up.railway.app', '127.0.0.1:8000']
 
 
 # Application definition

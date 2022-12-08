a48297d40d0b0c94e5f6f40d91663b59766c59f1
lalsharath511 <lalsharath511@gmail.com>
2022-12-08 15:52:31 +0530

    static problem

--- project/settings.py
+++ project/settings.py
@@ -117,10 +117,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = "static/"
 import os
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "static/"
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
